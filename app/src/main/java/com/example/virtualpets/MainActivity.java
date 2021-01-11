7b3beafa16ecef33b3d13abf3c68ecab100c07bf
Jason Chow <jason.chowcs@gmail.com>
2021-01-12 01:03:16 +0800

    Firebase without bonus

--- app/src/main/java/com/example/virtualpets/MainActivity.java
+++ app/src/main/java/com/example/virtualpets/MainActivity.java
@@ -1,24 +1,118 @@
 package com.example.virtualpets;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "Hello World");
 
         Button button = findViewById(R.id.startButton);
 
         button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, ChoosePetActivity1.class);
-            startActivity(intent);
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+
+
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // Asserts that this result came from our FirebaseUI
+
+            if (resultCode == RESULT_OK) {
+                // Means the user successfully signed in
+                // We can now get the details of the Firebase User
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Get the name of the user
+                String userName = user.getDisplayName();
+
+                // Show a message welcoming the user
+                Toast.makeText(this, "Welcome " + userName, Toast.LENGTH_SHORT).show();
+
+                // Get Signed-in user's id
+                String userId = user.getUid();
+                // Get Reference to database
+                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                // Get Reference to where we expect user's data to be
+                DatabaseReference mUser = mDatabase.child("pets_database").child(userId);
+                // Use this function to attempt to read data from our reference
+                mUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // Check whether user has does indeed have existing pet data
+                        if (snapshot.exists()) {
+                            // If data exists, we want to extract the pet types and hunger values
+                            // And store them into sharedPreferences
+
+                            SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                            sharedPref.edit()
+                                    .putString("pet1", snapshot.child("pet1").child("type").getValue(String.class))
+                                    .putInt("pet1hunger", snapshot.child("pet1").child("hunger").getValue(int.class))
+                                    .putString("pet2", snapshot.child("pet2").child("type").getValue(String.class))
+                                    .putInt("pet2hunger", snapshot.child("pet2").child("hunger").getValue(int.class))
+                                    .apply();
+
+                            // Go to MyPetActivity
+                            Intent intent = new Intent(MainActivity.this, MyPetActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Go to ChoosePetActivity
+                            Intent intent = new Intent(MainActivity.this, ChoosePetActivity1.class);
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            } else {
+                Toast.makeText(this, "Sign in Failed!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/virtualpets/MyPetActivity.java
+++ app/src/main/java/com/example/virtualpets/MyPetActivity.java
@@ -1,15 +1,42 @@
 package com.example.virtualpets;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MyPetActivity extends AppCompatActivity {
+    DatabaseReference mDatabase;
+    DatabaseReference mUserRef;
+    private final static int RC_PICK_IMAGE_1 = 1;
+    private final static int RC_PICK_IMAGE_2 = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +57,41 @@ public class MyPetActivity extends AppCompatActivity {
 
         // Get Pet1, default to cat
         String pet1 = sharedPref.getString("pet1", "cat");
+        int pet1HungerValue = sharedPref.getInt("pet1hunger", 100);
 
         // Get Pet2, default to dog
         String pet2 = sharedPref.getString("pet2", "dog");
+        int pet2HungerValue = sharedPref.getInt("pet2hunger", 100);
 
-        initialiseWidgetsForPet(pet1Image, pet1HungerText, pet1Button, pet1);
-        initialiseWidgetsForPet(pet2Image, pet2HungerText, pet2Button, pet2);
+        // Get a reference to our Firebase Realtime Database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Get our user's email
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Get reference to our user's pet data
+        mUserRef = mDatabase.child("pets_database").child(userId);
+
+        mUserRef.child("pet1").child("type").setValue(pet1);
+        mUserRef.child("pet1").child("hunger").setValue(pet1HungerValue);
+        mUserRef.child("pet2").child("type").setValue(pet2);
+        mUserRef.child("pet2").child("hunger").setValue(pet2HungerValue);
+
+        initialiseWidgetsForPet("pet1", pet1Image, RC_PICK_IMAGE_1, pet1HungerText, pet1Button, pet1, pet1HungerValue);
+        initialiseWidgetsForPet("pet2", pet2Image, RC_PICK_IMAGE_2, pet2HungerText, pet2Button, pet2, pet2HungerValue);
     }
 
-    private void initialiseWidgetsForPet(ImageView petImage, TextView hungerText,
-                                         Button petButton, String pet) {
+    private void initialiseWidgetsForPet(String petId, ImageView petImage,
+                                         int imageRC, TextView hungerText,
+                                         Button petButton, String pet, int petHungerValue) {
         // This function runs for each pet we have
+
+        petImage.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), imageRC);
+        });
 
         // Set the image based on which pet it is
         if (pet.equals("cat")) {
@@ -51,13 +102,92 @@ public class MyPetActivity extends AppCompatActivity {
             petImage.setImageResource(R.drawable.fish);
         }
 
-        // Set the hungerText to 100 at first
-        hungerText.setText(Integer.toString(100));
+        hungerText.setText(Integer.toString(petHungerValue));
+
+        mUserRef.child(petId).child("hunger").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                hungerText.setText(Integer.toString(snapshot.getValue(int.class)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        mUserRef.child(petId).child("imageUrl").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String imageUrlString = snapshot.getValue(String.class);
+                    setBitmapFromURL(imageUrlString, petImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         // Set onclicklistener to increment hunger by 1 for each button click
         petButton.setOnClickListener(view -> {
             int oldHunger = Integer.parseInt(hungerText.getText().toString());
             hungerText.setText(Integer.toString(oldHunger + 1));
+            mUserRef.child(petId).child("hunger").setValue(oldHunger + 1);
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_PICK_IMAGE_1 || requestCode == RC_PICK_IMAGE_2) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                StorageReference userStorageRef = storageRef.child(userId).child(Integer.toString(requestCode));
+                userStorageRef.putStream(inputStream)
+                    .continueWithTask(taskSnapshot -> {
+                        return userStorageRef.getDownloadUrl();
+                    }).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            String imageUrl = task.getResult().toString();
+                            mUserRef.child("pet" + requestCode).child("imageUrl").setValue(imageUrl);
+                        }
+                    });
+            } catch (FileNotFoundException ex) {
+            }
+        }
+    }
+
+    /**
+     * Generic function to set an ImageView from a given url
+     */
+    private void setBitmapFromURL(String imageString, ImageView imageView) {
+        new Thread(() -> {
+            // We will need to download the image, on a separate thread/process to avoid hanging our UI
+            try {
+                // We use try/catch since our download may not succeed
+                // First convert the url to a URL type object
+                URL imageURL = new URL(imageString);
+                // Set up connection to the URL
+                HttpURLConnection connection = (HttpURLConnection) imageURL.openConnection();
+                // Used to indicate we are using the URL connection for input
+                connection.setDoInput(true);
+                // Attempt to initiate the connection
+                connection.connect();
+                // Get an inputstream that will receive our Image
+                InputStream inputStream = connection.getInputStream();
+                // Convert the bytes received from the stream into a Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                // Now that we have our downloading Bitmap, we go back to the thread running the UI
+                // so we can finally set the bitmap to the ImageView
+                runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+            } catch (Exception ex) {
+            }
+        }).start();
     }
 }
